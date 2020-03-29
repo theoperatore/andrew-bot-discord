d22e37b25c56a1456cb207a72788443341bc84b5
Alex Petersen <theoperatore@gmail.com>
2020-03-29 12:49:34 -0400

    dont name it discord.js

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import * as api from './api';
-import * as discord from './discord';
+import * as andrewBot from './andrewBot';
 
 const PORT = process.env.PORT || 3000;
 const DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN || '';
 
 api.startServer(PORT);
-discord.startServer(DISCORD_BOT_TOKEN);
+andrewBot.startServer(DISCORD_BOT_TOKEN);
